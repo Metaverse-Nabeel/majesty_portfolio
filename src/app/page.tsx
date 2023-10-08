0703bf300603b7d1ea359e8c584f52e6b9c23488
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-10-08 18:59:13 +0500

    Test the dummy data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { Data } from '../data'
+import Image from 'next/image'
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Majesty Portfolio</h1>
+      <h1 className='text-2xl '>Majesty Portfolio</h1>
+      
+      
     </main>
   )
 }
