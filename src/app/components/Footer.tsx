d2f5e0491cf2e9371873fe65bfffc0f676e23f54
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-10-10 18:45:56 +0500

    Fix footer bug

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -9,10 +9,10 @@ const Footer: React.FC = () => {
     <footer className="w-full text-white bg-[#616161] pt-8">
       <div className="flex gap-8 px-6">
         <div className="flex flex-col">
-          <h4 className="text-3xl uppercase py-4">
-            Footer
+          <h4 className="text-3xl py-4">
+            Thank you for visiting my portfolio!
           </h4>
-          <p>Thank you for visiting my portfolio! I&apos;m passionate about creating meaningful and impactful experiences through design and development. Whether it&apos;s crafting intuitive user interfaces or diving into complex coding challenges, I thrive on turning ideas into reality. Let&apos;s connect and explore how we can collaborate to bring your projects to life. Feel free to reach out—I&apos;m always excited about new opportunities and creative ventures.</p>
+          <p>I&apos;m passionate about creating meaningful and impactful experiences through design and development. Whether it&apos;s crafting intuitive user interfaces or diving into complex coding challenges, I thrive on turning ideas into reality. Let&apos;s connect and explore how we can collaborate to bring your projects to life. Feel free to reach out—I&apos;m always excited about new opportunities and creative ventures.</p>
         </div>
         <div className="flex flex-col">
           <h4 className="text-3xl uppercase py-4">
@@ -26,7 +26,7 @@ const Footer: React.FC = () => {
         </div>
       </div>
       <div className="w-full h-20 bg-black flex items-center justify-center mt-8">
-        <p className="py-4 cursor-pointer">Made with <FontAwesomeIcon icon={faHeart} className="text-[#009688] hover:text-xl"/> by <Link href={'https://nabeel-digital.vercel.app/'} className="font-bold" target="blank">Nabeel Ahmed</Link></p>
+        <p className="py-4 cursor-pointer"><Link href={'https://nabeel-digital.vercel.app/'} target="blank"> Made with <FontAwesomeIcon icon={faHeart} className="hover:text-red-600 hover:text-xl"/> by Nabeel Ahmed</Link></p>
       </div>
     </footer>
   )
