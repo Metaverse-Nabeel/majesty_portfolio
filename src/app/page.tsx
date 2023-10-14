d30de416e0463bfe8fb5393bbe5d76a6d2a37d2f
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-10-14 15:58:28 +0500

    Responsiveness for the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,11 @@ import Contact from "./sections/Contact";
 export default function Home() {
   return (
     <>
-      <main className="w-full h-screen sm:bg-slate-300/40 sm:grid sm:grid-cols-10 sm:gap-2">
-        <section className="sm:col-span-2">
+      <main className="w-full h-screen bg-slate-300/40 lg:grid lg:grid-cols-10 lg:gap-2">
+        <section className="lg:col-span-2">
         <Sidebar />
         </section>
-        <section className="sm:col-span-8 bg-gray-100">
+        <section className="lg:col-span-8 bg-gray-100">
           <Portfolio />
           <AboutMe />
           <Packages/>
