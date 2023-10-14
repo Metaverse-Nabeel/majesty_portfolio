1f57a93bcc40503ae08610d0c4b8e4726ee55557
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-10-14 13:57:02 +0500

    Mobile navbar made for all screens

--- src/app/components/PortfolioHeader.tsx
+++ src/app/components/PortfolioHeader.tsx
@@ -11,14 +11,14 @@ const PortfolioHeader = () => {
 
   return (
     <>
-      <div className="flex flex-col">
+      <div className="flex flex-col md:mt-36">
         <h1 className="text-[36px] px-4 py-4 font-semibold">My Portfolio</h1>
-      <div className="flex space-x-1 items-center justify-start px-4 py-2 text-xl">
+      <div className="lg:flex md:flex md:flex-wrap sm:flex-wrap space-y-2 space-x-2 items-center justify-start px-4 py-2 text-xl">
         <p className="text-md mr-4">Filters:</p>
-        <button className={`px-5 py-2 ${category === "all" ? 'bg-black text-white' : 'bg-white text-black'} font-light text-black hover:bg-black hover:text-white uppercase`} onClick={() => dispatch(filterProjects("all"))}>All</button>
-        <button className={`px-4 py-2 ${category === "design" ? 'bg-black text-white' : 'bg-white text-black'} font-light text-black hover:bg-black hover:text-white`} onClick={() => dispatch(filterProjects("design"))}><FontAwesomeIcon icon={faGem} className="text-md pr-4"  />Design</button>
-        <button className={`px-4 py-2 ${category === "photos" ? 'bg-black text-white' : 'bg-white text-black'} font-light text-black hover:bg-black hover:text-white`} onClick={() => dispatch(filterProjects("photos"))}><FontAwesomeIcon icon={faImage} className="text-md pr-4"  />Photos</button>
-        <button className={`px-4 py-2 ${category === "art" ? 'bg-black text-white' : 'bg-white text-black'} font-light text-black hover:bg-black hover:text-white`} onClick={() => dispatch(filterProjects("art"))}><FontAwesomeIcon icon={faPaintBrush} className="text-md pr-4"  />Art</button>
+        <button className={`px-5 py-2 sm:my-2 ${category === "all" ? 'bg-black text-white' : 'bg-white text-black'} font-light text-black hover:bg-black hover:text-white uppercase`} onClick={() => dispatch(filterProjects("all"))}>All</button>
+        <button className={`px-4 py-2 sm:my-2 ${category === "design" ? 'bg-black text-white' : 'bg-white text-black'} font-light text-black hover:bg-black hover:text-white`} onClick={() => dispatch(filterProjects("design"))}><FontAwesomeIcon icon={faGem} className="text-md pr-4"  />Design</button>
+        <button className={`px-4 py-2 sm:my-2 ${category === "photos" ? 'bg-black text-white' : 'bg-white text-black'} font-light text-black hover:bg-black hover:text-white`} onClick={() => dispatch(filterProjects("photos"))}><FontAwesomeIcon icon={faImage} className="text-md pr-4"  />Photos</button>
+        <button className={`px-4 py-2 sm:my-2 ${category === "art" ? 'bg-black text-white' : 'bg-white text-black'} font-light text-black hover:bg-black hover:text-white`} onClick={() => dispatch(filterProjects("art"))}><FontAwesomeIcon icon={faPaintBrush} className="text-md pr-4"  />Art</button>
         </div>
         <div className="h-2 my-2 mx-4 mb-6 bg-gray-400/50 "/>
       </div>
